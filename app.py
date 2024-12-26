adb08196a77f4777dffe315410bbc605e006739e
redmoon0x <bluew25882gmail.com>
2024-12-26 18:39:54 +0530

    Add webhook support and render index.html template in Flask app

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, Response
 from astro_bot import AstroBot
 import threading
 import os
+import telebot
 
 app = Flask(__name__)
 bot = AstroBot()
 
 @app.route('/')
 def home():
-    return 'Bot is running!'
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.bot.process_new_updates([update])
+        return Response('ok', status=200)
+    else:
+        return Response(status=403)
 
 def run_bot():
     bot.run()
